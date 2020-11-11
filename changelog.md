50dd8fb7476a845f448f2a81cdfe1bdb583d6875
kyllian1212 <kyllian1212@live.fr>
2020-11-11 17:58:56 +0100

    v0.3.2

---
+++ changelog.md
@@ -0,0 +1,33 @@
+------------------------------------------------------------------------
+## v0.1.0 - 10/31/2020
+* initial release with report function
+## v0.1.1 - 10/31/2020
+* bugfix with role issues
+## v0.1.2 - 10/31/2020
+* added songs into the presence thing
+* bugfix with crash issue 
+
+------------------------------------------------------------------------
+## v0.2.0 - 11/01/2020 
+* added more songs into the library
+* added a proper crash handler
+## v0.2.1 - 11/01/2020 
+* converted the !!quit command into a proper bot.command() instead of a on_message() command
+* .gitignore update to exclude the python cache
+
+------------------------------------------------------------------------
+## v0.3.0 - 11/01/2020
+* added !!say command
+* added !!info command
+* added !!october18 command
+* transferred the version into an .env variable
+* the bot will now show when it's in dev mode (as in when it's running locally, rather than running on heroku)
+## v0.3.1 - 10/02/2020
+* fixed the !!info command (everyone used to have created the bot lmao)
+## v0.3.2 - 11/11/2020 
+* added a version verification (if versions aren't matching, the bot will quit)
+* added !!changelog
+* full changelog is now in a markdown format
+* fixed the issue with the bot crashing if people react in another server
+* fixed the issue with edited messages not being able to be reported
+
--- main.py
+++ main.py
@@ -16,14 +16,26 @@ import song_library as sl
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-VERSION = os.getenv('VERSION')
 
-bot = commands.Bot(command_prefix="!!")
+#program version in .env and for git. if they're not matching, the program will quit. 
+#if its a -dev version, it will load anyway but with a warning.
+VERSION = os.getenv('VERSION')
+GIT_VERSION = "v0.3.2"
+
+#dev mode is when i run the bot locally
+devmode = False
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!!", intents=intents)
 
 @bot.event
 async def on_ready():
     try:
+        global devmode
         now = datetime.now()
+
+        if str(VERSION).endswith("-dev"):
+            devmode = True
 
         for guild in bot.guilds:
             if guild.name == GUILD:
@@ -34,8 +46,32 @@ async def on_ready():
             f'{guild.name}(id: {guild.id})\n'
         )
 
-        members = '\n - '.join([member.name for member in guild.members])
-        print(f'Guild Members:\n - {members}')
+        membercount = 0
+        for member in guild.members:
+            membercount = membercount+1
+
+        print(f'Guild Member Count: {membercount}')
+
+        #version mismatch check
+        if VERSION != GIT_VERSION:
+            if devmode:
+                versionmismatch_message_embed = discord.Embed(title="warning: versions aren't matching", description="*the bot is in dev mode, so it will boot anyways.*", color=0xffae42)
+            else:
+                versionmismatch_message_embed = discord.Embed(title="error: versions aren't matching!!", color=0xff0000)
+                versionmismatch_message_embed.add_field(name="reason", value="this error can happen because:\n " + 
+                "- the .env value for 'version' isn't correctly set\n" +
+                "- the bot is running on the previous commit\n " +
+                "- the bot is in dev mode, but there's an issue with the devmode variable\n " +
+                "\nplease check the .env values or make sure the next commit is built. the bot will now shutdown."
+                , inline=False)
+            versionmismatch_message_embed.add_field(name=".env version", value="*"+VERSION+"*", inline=True)
+            versionmismatch_message_embed.add_field(name="git version", value="*"+GIT_VERSION+"*", inline=True)
+            versionmismatch_message_embed.set_footer(text=str(now.strftime("%d/%m/%Y - %H:%M:%S")))
+
+            await bot.get_channel(772219545082396692).send(embed=versionmismatch_message_embed)
+
+            if not devmode:
+                await os._exit(-1)
 
         #bot version to be changed here for now - v major.minor.bugfix-dev
         init_message_embed = discord.Embed(title="bot has successfully booted up.", description="*bot version: " + VERSION + "*", color=0x00aeff)
@@ -47,6 +83,7 @@ async def on_ready():
         await bot.get_channel(772219545082396692).send(embed=init_message_embed)
     except:
         await crash_handler()
+        await os._exit(-1)
         raise
 
 @bot.command()
@@ -66,7 +103,7 @@ async def quit(ctx):
             quit_message_embed.set_footer(text=str(now.strftime("%d/%m/%Y - %H:%M:%S")))
             await bot.get_channel(772219545082396692).send(embed=quit_message_embed)
             await bot.loop.stop()
-            await exit()
+            await os._exit(1)
         else:
             await mod_only_command(ctx)
     except:
@@ -115,13 +152,29 @@ async def october18(ctx):
         await crash_handler()
         raise
 
+@bot.command()
+async def changelog(ctx):
+    try:
+        now = datetime.now()
+        changelog = open('changelog_bot.txt', 'r').read()
+        changelog_message_embed = discord.Embed(title="Changelog", description=changelog, url="https://github.com/kyllian1212/Rin/blob/master/changelog.txt", color=0x00aeff)
+        changelog_message_embed.set_thumbnail(url=bot.user.avatar_url)
+        changelog_message_embed.set_footer(text=str(now.strftime("%d/%m/%Y - %H:%M:%S")) + "  •  full changelog available by clicking the link above")
+        await ctx.channel.send(embed=changelog_message_embed)
+    except:
+        await crash_handler()
+        raise
+
 
 @bot.event
 async def on_reaction_add(reaction, user):
     try:
         now = datetime.now()
         if reaction.count == 1:
-            channel_report = bot.get_channel(413488194819194891)
+            if devmode:
+                channel_report = bot.get_channel(775080183602085899)
+            else:
+                channel_report = bot.get_channel(413488194819194891)
             guild = bot.get_guild(186610204023062528)
             mod_role = guild.get_role(219977258453041152)
             reacted_message = reaction.message
@@ -132,57 +185,58 @@ async def on_reaction_add(reaction, user):
             mod_report = False
             description = ""
 
-            for role in reacted_message_author.roles:
-                if role.name != '@everyone':
-                    if reacted_message_author_roles is None:
-                        reacted_message_author_roles = str(role)
-                    else:
-                        reacted_message_author_roles = str(reacted_message_author_roles) + ", " + str(role)
-
-            
-            if reaction.emoji == '🚫':
-                #if a user auto reacts 🚫 to their message, it just deletes it without reporting it
-                if user == reacted_message_author:
-                    await reacted_message.delete()
-                else:
-                    for role in user.roles:
-                        if role == mod_role:
-                            #report_confirmation_embed = discord.Embed(title="The message has been reported.", color=0xff0000)
-                            await reacted_message.delete()
-                            mod_report = True
-                            description = "*The message has been deleted as it has been reported by an admin.*"
-                            #report_confirmation_message = await reacted_message.channel.send(embed=report_confirmation_embed)
-                            #await asyncio.sleep(2)
-                            #await report_confirmation_message.delete()
-
-                    reported_message_embed = discord.Embed(title="Reported Message - ID: " + str(reacted_message.id), description=description, color=0xff0000)
-                    reported_message_embed.set_thumbnail(url=reacted_message_author.avatar_url)
-                    reported_message_embed.add_field(name="Username", value=reacted_message_author.name, inline=True)
-                    reported_message_embed.add_field(name="Nickname", value=reacted_message_author.nick, inline=True)
-                    reported_message_embed.add_field(name="User ID", value="<@" + str(reacted_message_author.id) + ">", inline=True)
-                    reported_message_embed.add_field(name="Channel", value="<#" + str(reacted_message.channel.id) + ">", inline=True)
-                    reported_message_embed.add_field(name="User Roles", value=str(reacted_message_author_roles), inline=True)
-                    #if the message is too long
-                    if len(reacted_message_content) >= 1000:
-                        long_message = True
-                        message_length = len(reacted_message_content)
-                        reacted_message_content_part2 = reacted_message_content[999:message_length]
-                        reacted_message_content = reacted_message_content[0:999]
-                        reported_message_embed.add_field(name="Message", value=reacted_message_content, inline=False)
-                        reported_message_part2_embed = discord.Embed(description="*The message is over 1000 characters, so it has been split into 2 Discord embeds.*", color=0xff0000)
-                        reported_message_part2_embed.add_field(name="Message (part 2)", value=reacted_message_content_part2, inline=False)
-                        reported_message_part2_embed.set_footer(text="Reported by " + str(user.name) + "#" + str(user.discriminator) + "  •  " + str(now.strftime("%d/%m/%Y - %H:%M:%S")), icon_url=user.avatar_url)
-                    else:
-                        #if the message only contains an attachment
-                        if len(reacted_message_content) != 0:
-                            reported_message_embed.add_field(name="Message", value=reacted_message_content, inline=False)
+            if user.guild == guild:
+                for role in reacted_message_author.roles:
+                    if role.name != '@everyone':
+                        if reacted_message_author_roles is None:
+                            reacted_message_author_roles = str(role)
                         else:
-                            reported_message_embed.add_field(name="Attachment", value="*The reported message only contained an attachment; make sure to have it saved before reporting it as the bot cannot currently save (deleted) pictures. You can also give out a description of the attachment below*", inline=False)
-                        reported_message_embed.set_footer(text="Reported by " + str(user.name) + "#" + str(user.discriminator) + "  •  " + str(now.strftime("%d/%m/%Y - %H:%M:%S")), icon_url=user.avatar_url)
+                            reacted_message_author_roles = str(reacted_message_author_roles) + ", " + str(role)
 
-                    await channel_report.send(embed=reported_message_embed)
-                    if long_message == True:
-                        await channel_report.send(embed=reported_message_part2_embed)
+                
+                if reaction.emoji == '🚫':
+                    #if a user auto reacts 🚫 to their message, it just deletes it without reporting it
+                    if user == reacted_message_author:
+                        await reacted_message.delete()
+                    else:
+                        for role in user.roles:
+                            if role == mod_role:
+                                #report_confirmation_embed = discord.Embed(title="The message has been reported.", color=0xff0000)
+                                await reacted_message.delete()
+                                mod_report = True
+                                description = "*The message has been deleted as it has been reported by an admin.*"
+                                #report_confirmation_message = await reacted_message.channel.send(embed=report_confirmation_embed)
+                                #await asyncio.sleep(2)
+                                #await report_confirmation_message.delete()
+
+                        reported_message_embed = discord.Embed(title="Reported Message - ID: " + str(reacted_message.id), description=description, color=0xff0000)
+                        reported_message_embed.set_thumbnail(url=reacted_message_author.avatar_url)
+                        reported_message_embed.add_field(name="Username", value=reacted_message_author.name, inline=True)
+                        reported_message_embed.add_field(name="Nickname", value=reacted_message_author.nick, inline=True)
+                        reported_message_embed.add_field(name="User ID", value="<@" + str(reacted_message_author.id) + ">", inline=True)
+                        reported_message_embed.add_field(name="Channel", value="<#" + str(reacted_message.channel.id) + ">", inline=True)
+                        reported_message_embed.add_field(name="User Roles", value=str(reacted_message_author_roles), inline=True)
+                        #if the message is too long
+                        if len(reacted_message_content) >= 1000:
+                            long_message = True
+                            message_length = len(reacted_message_content)
+                            reacted_message_content_part2 = reacted_message_content[999:message_length]
+                            reacted_message_content = reacted_message_content[0:999]
+                            reported_message_embed.add_field(name="Message", value=reacted_message_content, inline=False)
+                            reported_message_part2_embed = discord.Embed(description="*The message is over 1000 characters, so it has been split into 2 Discord embeds.*", color=0xff0000)
+                            reported_message_part2_embed.add_field(name="Message (part 2)", value=reacted_message_content_part2, inline=False)
+                            reported_message_part2_embed.set_footer(text="Reported by " + str(user.name) + "#" + str(user.discriminator) + "  •  " + str(now.strftime("%d/%m/%Y - %H:%M:%S")), icon_url=user.avatar_url)
+                        else:
+                            #if the message only contains an attachment
+                            if len(reacted_message_content) != 0:
+                                reported_message_embed.add_field(name="Message", value=reacted_message_content, inline=False)
+                            else:
+                                reported_message_embed.add_field(name="Attachment", value="*The reported message only contained an attachment; make sure to have it saved before reporting it as the bot cannot currently save (deleted) pictures. You can also give out a description of the attachment below*", inline=False)
+                            reported_message_embed.set_footer(text="Reported by " + str(user.name) + "#" + str(user.discriminator) + "  •  " + str(now.strftime("%d/%m/%Y - %H:%M:%S")), icon_url=user.avatar_url)
+
+                        await channel_report.send(embed=reported_message_embed)
+                        if long_message == True:
+                            await channel_report.send(embed=reported_message_part2_embed)
 
     #put crash handler in another function   
     except:
